51d289f461428791073aac81ede09eaf68dd1510
Marawan-Osama <marawanegy@gmail.com>
2023-10-07 18:50:30 +0300

    posting and getting all jobs

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import * as dotenv from 'dotenv';
+import { nanoid } from 'nanoid';
 
 dotenv.config();
 const app = express();
@@ -9,6 +10,12 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const jobs = [
+  { id: nanoid(), company: 'google', position: 'backend' },
+  { id: nanoid(), company: 'apple', position: 'frontend' },
+  { id: nanoid(), company: 'netflix', position: 'midend lol' },
+];
 
 app.get('/', (req, res) => {
   res.json({ msg: 'hello' });
@@ -19,6 +26,24 @@ app.post('/', (req, res) => {
   res.json({ message: 'data recieved', data: req.body });
 });
 
-app.listen(5001, () => {
-  console.log('listening on port 5001');
+app.get('/api/v1/jobs', (req, res) => {
+  res.status(200).json({ jobs });
+});
+
+app.post('/api/v1/jobs', (req, res) => {
+  const { company, position } = req.body;
+  if (!company || !position) {
+    return res
+      .status(400)
+      .json({ msg: 'Please provide correct data (company and position)' });
+  }
+  const id = nanoid();
+  const job = { id: id, company: company, position: position };
+  jobs.push(job);
+  res.status(201).json({ job });
+});
+
+const port = process.env.PORT || 5001;
+app.listen(process.env.PORT, () => {
+  console.log(`listening on port ${port}`);
 });
