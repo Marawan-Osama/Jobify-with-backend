be9daaeb7f9e770cf4ff9dea59543104c79b5d46
Marawan-Osama <marawanegy@gmail.com>
2023-10-07 17:53:57 +0300

    setting up backend

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-console.log('working server');
+import express from 'express';
+import morgan from 'morgan';
+const app = express();
+
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  res.json({ msg: 'hello' });
+});
+
+app.post('/', (req, res) => {
+  console.log(req);
+  res.json({ message: 'data recieved', data: req.body });
+});
+
+app.listen(5001, () => {
+  console.log('listening on port 5001');
+});
