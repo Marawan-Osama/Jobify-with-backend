fc813a1d00863b7459a348daafe6d567bfd2a504
Marawan-Osama <marawanegy@gmail.com>
2023-10-08 16:26:47 +0300

    getAllJobs, createJob and getSingleJob all work and are connected to database

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,4 +1,5 @@
 import { nanoid } from 'nanoid';
+import Job from '../models/jobModel.js';
 
 //for ensuring everything works.. WILL BE REMOVED
 let jobs = [
@@ -8,12 +9,13 @@ let jobs = [
 ];
 
 export const getAllJobs = async (req, res) => {
+  const jobs = await Job.find({});
   res.status(200).json({ jobs });
 };
 
 export const getSingleJob = async (req, res) => {
   const { id } = req.params;
-  const job = jobs.find((job) => job.id === id);
+  const job = await Job.findById(id);
   if (!job) {
     return res.status(404).json({ msg: `No job with id ${id}` });
   }
@@ -37,15 +39,7 @@ export const patchJob = async (req, res) => {
 };
 
 export const createJob = async (req, res) => {
-  const { company, position } = req.body;
-  if (!company || !position) {
-    return res
-      .status(400)
-      .json({ msg: 'Please provide correct data (company and position)' });
-  }
-  const id = nanoid();
-  const job = { id: id, company: company, position: position };
-  jobs.push(job);
+  const job = await Job.create(req.body);
   res.status(201).json({ job });
 };
 
