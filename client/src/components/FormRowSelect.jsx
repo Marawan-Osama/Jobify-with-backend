9c47a60231e834e347dd4e6da4447a14249de226
Marawan-Osama <marawanegy@gmail.com>
2023-10-10 14:09:15 +0300

    commented component FormRow

--- client/src/components/FormRowSelect.jsx
+++ client/src/components/FormRowSelect.jsx
@@ -1,3 +1,10 @@
+//this component is used to create a form row with a select input
+//each prop does the following:
+//name: the name of the input
+//labelText: the text to display in the label
+//list: an array of values to display as options
+//defaultValue: the default value to display in the select input
+
 const FormRowSelect = ({ name, labelText, list, defaultValue = '' }) => {
   return (
     <div className='form-row'>
