bc37f8845ad35feb88af7fc1a6a71e5f5fcca989
Marawan-Osama <marawanegy@gmail.com>
2023-10-11 20:38:10 +0300

    build created and ready

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -24,16 +24,11 @@ const Register = () => {
         <Logo />
         <h4>Register</h4>
 
-        <FormRow type='text' name='name' defaultValue='Marawan' />
-        <FormRow
-          type='text'
-          name='lastName'
-          labelText='Last Name'
-          defaultValue='Osama'
-        />
-        <FormRow type='text' name='location' defaultValue='Egypt' />
-        <FormRow type='email' name='email' defaultValue='marawan@gmail.com' />
-        <FormRow type='password' name='password' defaultValue='secret123' />
+        <FormRow type='text' name='name' />
+        <FormRow type='text' name='lastName' labelText='Last Name' />
+        <FormRow type='text' name='location' />
+        <FormRow type='email' name='email' />
+        <FormRow type='password' name='password' />
 
         <SubmitBtn />
         <p>
