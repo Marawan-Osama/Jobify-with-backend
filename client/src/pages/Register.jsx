18e411dc37e471c6a2d16603d0fe010b12568ad0
Marawan-Osama <marawanegy@gmail.com>
2023-10-12 15:55:52 +0300

    commented register

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -4,6 +4,8 @@ import { FormRow, Logo, SubmitBtn } from '../components';
 import customFetch from '../utils/customFetch';
 import { toast } from 'react-toastify';
 
+//this action is called when the form is submitted, it sends a post request to the server
+//and if the request is successful, it redirects the user to the login page
 export const action = async ({ request }) => {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
