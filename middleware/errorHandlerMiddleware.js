09831783826cd479dc61c170ae8eb71484996ed9
Marawan-Osama <marawanegy@gmail.com>
2023-10-20 01:12:19 +0300

    commented errorHandlerMiddleware

--- middleware/errorHandlerMiddleware.js
+++ middleware/errorHandlerMiddleware.js
@@ -1,5 +1,7 @@
 import { StatusCodes } from 'http-status-codes';
 
+//this middleware will handle all the errors in the app.
+//its function is to send a response with the error message and the status code rather than crashing the app or sending a response with a generic message
 const errorHandlerMiddleware = (err, req, res, next) => {
   console.log(err);
   const statusCode = err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR;
